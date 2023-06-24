666af102f2069aae6984d3286d4c1886ce60be1c
wangwei <412390898@qq.com>
2023-06-25 00:40:59 +0800

    学生管理系统完成

--- itheima-edu-info-manager/src/com/itheima/edu/info/manager/dao/StudentDao.java
+++ itheima-edu-info-manager/src/com/itheima/edu/info/manager/dao/StudentDao.java
@@ -3,15 +3,16 @@ package com.itheima.edu.info.manager.dao;
 import com.itheima.edu.info.manager.domain.Student;
 
 public class StudentDao {
+    //1.创建学生对象数组
+    private static Student[] students = new  Student[5];
     public boolean addStudent(Student stu) {
-        //1.创建学生对象数组
-        Student[] students = new  Student[5];
+
         //2.添加学生到数组
         //2.1 定义变量index为-1，假设数组已经全部存满，没有null的元素
         int index = -1;
         //2.2 遍历数组取出每一个元素，判断是否是null
         for (int i = 0; i < students.length; i++) {
-            Student student = new Student();
+            Student student = students[i];
             //2.3 如果为null，让index变量记录当前索引位置，并使用break结束循环遍历
             if(null==student){
                 index = i;
@@ -28,6 +29,34 @@ public class StudentDao {
             students[index]=stu;
             return true;
         }
+
+    }
+
+    public Student[] findAllStudent() {
+        return  students;
+    }
+
+    public void deleteStudentById(String delId) {
+        int index = getIndex(delId);
+        students[index] =null;
+    }
+    public int getIndex(String id){
+        int index = -1;
+        for (int i = 0; i < students.length; i++) {
+            Student student = students[i];
+            if(student != null && student.getId().equals(id)){
+                index= i;
+            break;
+            }
+        }
+        return  index;
+    }
+
+    public void updateStudent(String updateId, Student newStudent) {
+        //1.查找updateId索引位置
+        int index = getIndex(updateId);
+        //2.将该索引位置，使用新的学生对象代替
+        students[index] = newStudent;
 
     }
 }
--- itheima-edu-info-manager/src/com/itheima/edu/info/manager/service/StudentService.java
+++ itheima-edu-info-manager/src/com/itheima/edu/info/manager/service/StudentService.java
@@ -4,13 +4,56 @@ import com.itheima.edu.info.manager.dao.StudentDao;
 import com.itheima.edu.info.manager.domain.Student;
 
 public class StudentService {
+    private StudentDao studentDao = new StudentDao();
     public boolean addStudent(Student student) {
 //        1.创建StudentDao(库管)
-        StudentDao studentDao = new StudentDao();
+        //studentDao = new StudentDao();
 //        2.将学生对象，传递给StudentDao中的addStudent()方法
-       // boolean result = studentDao.addStudent(student);
+        // boolean result = studentDao.addStudent(student);
 //        3.将返回的boolean类型结果返还给StudentController
         //return  result;
         return studentDao.addStudent(student);
+    }
+
+    public boolean isExists(String id) {
+
+        Student[] students = studentDao.findAllStudent();
+        //默认id在数组中不存在
+        boolean exists = false;
+        for (int i = 0; i < students.length; i++) {
+            Student student = students[i];
+            if (student != null && student.getId().equals(id)) {
+                exists = true;
+                break;
+            }
+        }
+        return exists;
+    }
+
+    public Student[] findAllStudent() {
+        //查询学生全部信息
+        Student[] allStudent = studentDao.findAllStudent();
+        boolean flag = false;
+        for (int i = 0; i < allStudent.length; i++) {
+            Student student = allStudent[i];
+            if (student!=null){
+                flag = true;
+                break;
+            }
+        }
+        if (flag){
+            return  allStudent;
+        }else{
+            return null;
+        }
+
+    }
+
+    public void deleteStudentById(String delId) {
+        studentDao.deleteStudentById(delId);
+    }
+
+    public void updateStudent(String updateId, Student newStudent) {
+        studentDao.updateStudent(updateId,newStudent);
     }
 }
