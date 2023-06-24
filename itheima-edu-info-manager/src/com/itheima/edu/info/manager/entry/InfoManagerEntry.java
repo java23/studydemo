8f4d333a081c0075a636f9cb116f3a27c1be6405
wangwei <412390898@qq.com>
2023-06-24 17:41:01 +0800

    学生管理菜单搭建完成

--- itheima-edu-info-manager/src/com/itheima/edu/info/manager/entry/InfoManagerEntry.java
+++ itheima-edu-info-manager/src/com/itheima/edu/info/manager/entry/InfoManagerEntry.java
@@ -1,7 +1,39 @@
 package com.itheima.edu.info.manager.entry;
 
+import com.itheima.edu.info.manager.controller.StudentController;
+
+import java.util.Scanner;
+
 public class InfoManagerEntry {
     public static void main(String[] args) {
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            //菜单搭建
+            System.out.println("--------欢迎来到黑马信息管理系统--------");
+            System.out.println("请输入您的选择: 1.学生管理  2.老师管理  3.退出");
+            String choice = scanner.next();
+            switch (choice){
+                case "1" :
+                    System.out.println("学生管理");
+                    StudentController studentController = new StudentController();
+                    studentController.start();
+
+                    break;
+                case "2" :
+                    System.out.println("老师管理");
+                    break;
+                case "3" :
+                    System.out.println("感谢您的使用再见！");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("您的输入有误，请重新输入");
+                    break;
+            }
+        }
+
+
+
 
     }
 }
